40b046debe11a501aff29ae111b50734b26aaf75
Vilmar Batista <vilmarphp@gmail.com>
2021-06-16 11:18:21 -0300

    Feature: 😉 Challenged concluded

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,3 +1,51 @@
-export function Content() {
-  // Complete aqui
+import { FC, useState } from 'react';
+import '../styles/content.scss';
+import { MovieCard } from './MovieCard';
+
+interface MovieProps {
+  imdbID: string;
+  Title: string;
+  Poster: string;
+  Ratings: Array<{
+    Source: string;
+    Value: string;
+  }>;
+  Runtime: string;
 }
+
+interface GenreResponseProps {
+  id: number;
+  name: 'action' | 'comedy' | 'documentary' | 'drama' | 'horror' | 'family';
+  title: string;
+}
+
+interface IPropsContent {
+  selectedGenre: GenreResponseProps;
+  movies: MovieProps[];
+}
+
+export const Content: FC<IPropsContent> = ({ selectedGenre, movies }) => {
+  return (
+    <div className="container">
+      <header>
+        <span className="category">
+          Categoria:<span> {selectedGenre.title}</span>
+        </span>
+      </header>
+
+      <main>
+        <div className="movies-list">
+          {movies.map((movie) => (
+            <MovieCard
+              key={movie.imdbID}
+              title={movie.Title}
+              poster={movie.Poster}
+              runtime={movie.Runtime}
+              rating={movie.Ratings[0].Value}
+            />
+          ))}
+        </div>
+      </main>
+    </div>
+  );
+};
--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,47 @@
-export function SideBar() {
-  // Complete aqui
+import { FC, useEffect, useState } from 'react';
+import { api } from '../services/api';
+import '../styles/sidebar.scss';
+import { Button } from './Button';
+
+export interface GenreResponseProps {
+  id: number;
+  name: 'action' | 'comedy' | 'documentary' | 'drama' | 'horror' | 'family';
+  title: string;
 }
+
+interface IPropsSidebar {
+  onClick(id: number): void;
+  selectedGenreId: number;
+}
+
+export const SideBar: FC<IPropsSidebar> = ({
+  onClick: handleClick,
+  selectedGenreId,
+}) => {
+  const [genres, setGenres] = useState<GenreResponseProps[]>([]);
+  useEffect(() => {
+    api.get<GenreResponseProps[]>('genres').then((response) => {
+      setGenres(response.data);
+    });
+  }, []);
+
+  return (
+    <nav className="sidebar">
+      <span>
+        Watch<p>Me</p>
+      </span>
+
+      <div className="buttons-container">
+        {genres.map((genre) => (
+          <Button
+            key={`${genre.id}`}
+            title={genre.title}
+            iconName={genre.name}
+            onClick={() => handleClick(genre.id)}
+            selected={selectedGenreId === genre.id}
+          />
+        ))}
+      </div>
+    </nav>
+  );
+};
